c9d5de4b0c068095cb7e543a5e4eac955e848e0e
PeriClesII <60017068+pericles842@users.noreply.github.com>
2024-04-10 11:39:12 -0400

    Creacion de tablas
    Se crearon las tablas 
    almacenes
    tiendas
    usuario_almacen
    usuario_tienda

--- database/migrations/2024_03_30_045050_inventario_v1.php
+++ database/migrations/2024_03_30_045050_inventario_v1.php
@@ -59,6 +59,58 @@ class InventarioV1 extends Migration
             ->onUpdate('cascade');
         });
         DB::statement("CALL create_timestamps('usuarios')");
+
+
+        //TABLA ESTATUS
+        Schema::create('esatus', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 300)->nullable(false)->comment('Nombre del estatus ');
+        });
+        DB::statement("CALL create_timestamps('esatus')");
+        
+        //TABLA ALMACENES
+        Schema::create('almacenes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 300)->nullable(false)->comment('Nombre del tiendas ');
+            $table->string('direction', 500)->nullable(false)->comment('Direccion del alamacen ');
+            $table->foreignId('status_id')->nullable(false)->comment('id del estatus')
+            ->references('id')->on('esatus')
+            ->onDelete('restrict')
+            ->onUpdate('cascade');
+        });
+        DB::statement("CALL create_timestamps('almacenes')");
+
+        //TABLA TIENDAS
+        Schema::create('tiendas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 300)->nullable(false)->comment('Nombre del tiendas ');
+            $table->string('direction', 500)->nullable(false)->comment('Direccion del tiendas ');
+            $table->foreignId('status_id')->nullable(false)->comment('id del estatus')
+            ->references('id')->on('esatus')
+            ->onDelete('restrict')
+            ->onUpdate('cascade');
+        });
+        DB::statement("CALL create_timestamps('tiendas')");
+
+        //TABLA RELACION USUARIOS ALMACENES
+        Schema::create('usuario_almacen', function (Blueprint $table) {
+            $table->foreignId('user_id')->nullable(false)->comment('id del usuario')
+            ->references('id')->on('usuarios')->onDelete('cascade')->onUpdate('cascade');
+            
+            $table->foreignId('almacen_id')->nullable(false)->comment('id del alamcen')
+            ->references('id')->on('almacenes')->onDelete('cascade')->onUpdate('cascade');
+        });
+        DB::statement("CALL create_timestamps('usuario_almacen')");
+
+        //TABLA RELACION USUARIOS TIENDAS
+        Schema::create('usuario_tienda', function (Blueprint $table) {
+            $table->foreignId('user_id')->nullable(false)->comment('id del usuario')
+            ->references('id')->on('usuarios')->onDelete('cascade')->onUpdate('cascade');
+            
+            $table->foreignId('tienda_id')->nullable(false)->comment('id de la tienda')
+            ->references('id')->on('tiendas')->onDelete('cascade')->onUpdate('cascade');
+        });
+        DB::statement("CALL create_timestamps('tiendas')");
     }
 
     /**
